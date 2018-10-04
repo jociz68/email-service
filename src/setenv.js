dc3cb0d4b64c0fcf5aa658ed1388ec1f7f831d48
Marák József <marak.jozsef@grape.hu>
2018-10-04 15:47:42 +0200

    Add environmet setter for RabbitMQ support

--- src/setenv.js
+++ src/setenv.js
@@ -1,5 +1,5 @@
 process.env['TEMPLATES_DIR'] = 'D:\\repos\\email-service\\example_templates';
-process.env['TRANSPORT'] = 'postmark';
+// process.env['TRANSPORT'] = 'postmark';
 process.env['TRANSPORT'] = 'smtp';
 process.env['POSTMARK_API_KEY'] = 'c8ff5232-7a00-4a95-9b70-b210a6fb26bf';
 process.env['RABBITMQ_QUEUE'] = 'task_queue1';
